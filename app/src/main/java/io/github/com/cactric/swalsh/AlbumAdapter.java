c48b19c1aaa059666d6a79a20b2ca21bfb0dfd32
Cactric <59334627+Cactric@users.noreply.github.com>
2024-06-15 12:50:55 +0100

    More work on recycler view

--- app/src/main/java/io/github/com/cactric/swalsh/AlbumAdapter.java
+++ app/src/main/java/io/github/com/cactric/swalsh/AlbumAdapter.java
@@ -3,6 +3,10 @@ package io.github.com.cactric.swalsh;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,8 +14,16 @@ import androidx.recyclerview.widget.RecyclerView;
 public class AlbumAdapter extends RecyclerView.Adapter<AlbumAdapter.ViewHolder> {
     private String[] pictures;
     public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final ImageView imageView;
+        private final TextView length_text;
+        private final ImageButton shareButton;
+        private final ImageButton deleteButton;
         public ViewHolder(View view) {
             super(view);
+            imageView = view.findViewById(R.id.album_picture);
+            length_text = view.findViewById(R.id.album_length_text);
+            shareButton = view.findViewById(R.id.album_share_button);
+            deleteButton = view.findViewById(R.id.album_delete_button);
         }
     }
 
