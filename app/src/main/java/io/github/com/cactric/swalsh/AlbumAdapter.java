6d767911d2e427693a3960bcc0d0195abe1543f2
Cactric <59334627+Cactric@users.noreply.github.com>
2024-06-15 15:08:54 +0100

    More recycler adapter stuff

--- app/src/main/java/io/github/com/cactric/swalsh/AlbumAdapter.java
+++ app/src/main/java/io/github/com/cactric/swalsh/AlbumAdapter.java
@@ -1,9 +1,9 @@
 package io.github.com.cactric.swalsh;
 
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -11,24 +11,42 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import kotlin.NotImplementedError;
+
 public class AlbumAdapter extends RecyclerView.Adapter<AlbumAdapter.ViewHolder> {
-    private String[] pictures;
+    private Uri[] pictures;
     public static class ViewHolder extends RecyclerView.ViewHolder {
         private final ImageView imageView;
-        private final TextView length_text;
+        private final TextView lengthText;
         private final ImageButton shareButton;
         private final ImageButton deleteButton;
         public ViewHolder(View view) {
             super(view);
             imageView = view.findViewById(R.id.album_picture);
-            length_text = view.findViewById(R.id.album_length_text);
+            lengthText = view.findViewById(R.id.album_length_text);
             shareButton = view.findViewById(R.id.album_share_button);
             deleteButton = view.findViewById(R.id.album_delete_button);
+        }
+
+        public ImageView getImageView() {
+            return imageView;
+        }
+
+        public TextView getLengthText() {
+            return lengthText;
+        }
+
+        public ImageButton getShareButton() {
+            return shareButton;
+        }
+
+        public ImageButton getDeleteButton() {
+            return deleteButton;
         }
     }
 
     // Takes an array of file paths to the pictures that should be displayed
-    public AlbumAdapter(String[] pictures) {
+    public AlbumAdapter(Uri[] pictures) {
         this.pictures = pictures;
     }
 
@@ -44,6 +62,14 @@ public class AlbumAdapter extends RecyclerView.Adapter<AlbumAdapter.ViewHolder> 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         // TODO: Update views
+        holder.getImageView().setImageURI(pictures[position]);
+        holder.getLengthText().setText("AAA");
+        holder.getShareButton().setOnClickListener(v -> {
+            throw new NotImplementedError("Sharing not implemented yet");
+        });
+        holder.getDeleteButton().setOnClickListener(v -> {
+            throw new NotImplementedError("Deleting not implemented yet");
+        });
     }
 
     @Override
