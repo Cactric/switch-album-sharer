426a11fcf4f90531caec37ae3aec7bf410466129
Cactric <59334627+Cactric@users.noreply.github.com>
2024-06-16 16:42:46 +0100

    Make manual entry dialog work I think

--- app/src/main/java/io/github/com/cactric/swalsh/ManualFragment.java
+++ app/src/main/java/io/github/com/cactric/swalsh/ManualFragment.java
@@ -5,7 +5,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.navigation.NavController;
@@ -40,6 +42,26 @@ public class ManualFragment extends Fragment {
                     activity.getSupportFragmentManager().findFragmentById(R.id.mainFragmentContainer);
             NavController navController = navHostFragment.getNavController();
 
+            // Do some validation
+            if (!ssidEditText.getText().toString().startsWith("switch_")) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+                // TODO: string resource
+                builder.setMessage("The SSID (Wifi name) should start with 'switch_'.\nLong-press the connect button to bypass this check");
+                builder.setPositiveButton(android.R.string.ok, (dialog, which) -> dialog.dismiss());
+                builder.setOnDismissListener(dialog -> {});
+                builder.create().show();
+                return;
+            }
+            if (passEditText.getText().length() != 8) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+                // TODO: string resource
+                builder.setMessage("The password should be 8 characters long.\nLong-press the connect button to bypass this check");
+                builder.setPositiveButton(android.R.string.ok, (dialog, which) -> dialog.dismiss());
+                builder.setOnDismissListener(dialog -> {});
+                builder.create().show();
+                return;
+            }
+
             Bundle bundle = new Bundle();
             // Construct the same kind of string that would've been in the code
             String sb = "WIFI:" + "S:" +
@@ -50,7 +72,38 @@ public class ManualFragment extends Fragment {
                     ";;"; // blank field would be hidden (or not)
 
             bundle.putString("scanned_data", sb);
-            navController.navigate(R.id.action_destination_scan_to_connectFragment, bundle);
+            navController.navigate(R.id.action_manual_to_connect, bundle);
+        });
+        root.findViewById(R.id.manual_submit).setOnLongClickListener(v -> {
+            // Long press for looser validation
+            // (in case something changes)
+            NavHostFragment navHostFragment = (NavHostFragment)
+                    activity.getSupportFragmentManager().findFragmentById(R.id.mainFragmentContainer);
+            NavController navController = navHostFragment.getNavController();
+
+            if (ssidEditText.getText().length() < 1) {
+                // TODO: String resource
+                Toast.makeText(activity, "You need to put in an SSID", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+            if (passEditText.getText().length() < 1) {
+                // TODO: string resource
+                Toast.makeText(activity, "You need to put in a password", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+
+            Bundle bundle = new Bundle();
+            // Construct the same kind of string that would've been in the code
+            String sb = "WIFI:" + "S:" +
+                    ssidEditText.getText() +
+                    ";T:WPA" + // just assume it's WPA2
+                    ";P:" +
+                    passEditText.getText() +
+                    ";;"; // blank field would be hidden (or not)
+
+            bundle.putString("scanned_data", sb);
+            navController.navigate(R.id.action_manual_to_connect, bundle);
+            return true;
         });
         return root;
     }
