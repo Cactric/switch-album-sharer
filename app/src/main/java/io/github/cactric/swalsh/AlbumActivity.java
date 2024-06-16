817a0ba43195419adce6c667a13aef50be7b4cd7
Cactric <59334627+Cactric@users.noreply.github.com>
2024-06-16 22:35:02 +0100

    Do some theming

--- app/src/main/java/io/github/cactric/swalsh/AlbumActivity.java
+++ app/src/main/java/io/github/cactric/swalsh/AlbumActivity.java
@@ -20,6 +20,7 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.color.DynamicColors;
 import com.google.android.material.tabs.TabLayout;
 
 import java.io.IOException;
--- app/src/main/java/io/github/cactric/swalsh/MainActivity.java
+++ app/src/main/java/io/github/cactric/swalsh/MainActivity.java
@@ -11,12 +11,20 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.google.android.material.color.DynamicColors;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EdgeToEdge.enable(this);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_linear_layout), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
