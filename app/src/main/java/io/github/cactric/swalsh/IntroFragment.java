f83f24adc1e74079effc46ae460c625cd9f7dae2
Cactric <59334627+Cactric@users.noreply.github.com>
2024-07-23 22:39:52 +0100

    Add an info button on the intro screen

--- app/src/main/java/io/github/cactric/swalsh/IntroFragment.java
+++ app/src/main/java/io/github/cactric/swalsh/IntroFragment.java
@@ -3,16 +3,22 @@ package io.github.cactric.swalsh;
 import android.Manifest;
 import android.app.ActivityOptions;
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.core.view.MenuProvider;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
 import android.transition.TransitionInflater;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
@@ -39,6 +45,21 @@ public class IntroFragment extends Fragment {
             storedNavController = Navigation.findNavController(v);
             requestPermLauncher.launch(Manifest.permission.CAMERA);
         });
+        requireActivity().addMenuProvider(new MenuProvider() {
+            @Override
+            public void onCreateMenu(@NonNull Menu menu, @NonNull MenuInflater menuInflater) {
+                menuInflater.inflate(R.menu.main_menu, menu);
+            }
+
+            @Override
+            public boolean onMenuItemSelected(@NonNull MenuItem menuItem) {
+                if (menuItem.getItemId() == R.id.info_option) {
+                    Toast.makeText(getContext(), "Not implemented yet", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+                return false;
+            }
+        }, getViewLifecycleOwner());
 
         root.findViewById(R.id.intro_manual_button).setOnClickListener(v -> Navigation.findNavController(v).navigate(R.id.action_manual_entry));
 
--- app/src/main/java/io/github/cactric/swalsh/MainActivity.java
+++ app/src/main/java/io/github/cactric/swalsh/MainActivity.java
@@ -1,13 +1,23 @@
 package io.github.cactric.swalsh;
 
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.navigation.ui.ToolbarKt;
 
 public class MainActivity extends AppCompatActivity {
 
