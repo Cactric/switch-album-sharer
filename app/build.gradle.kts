cf3a9a90ff2f60518b56d22a7798cce842e824b5
Cactric <59334627+Cactric@users.noreply.github.com>
2025-02-04 20:41:50 +0000

    Bump compileSdk and dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "io.github.cactric.swalsh"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "io.github.cactric.swalsh"
