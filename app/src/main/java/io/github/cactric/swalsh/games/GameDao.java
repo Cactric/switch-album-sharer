5a02056aab4e51d08aa64f691dd5b2f4e57f00de
Cactric <59334627+Cactric@users.noreply.github.com>
2025-09-04 18:05:11 +0100

    Remove unused GameDao method (specifically findByPrimaryKey)

--- app/src/main/java/io/github/cactric/swalsh/games/GameDao.java
+++ app/src/main/java/io/github/cactric/swalsh/games/GameDao.java
@@ -14,9 +14,6 @@ public interface GameDao {
     @Query("SELECT * FROM game")
     List<Game> getAll();
 
-    @Query("SELECT * FROM game WHERE game_primary_key == :primaryKey")
-    Game findByPrimaryKey(int primaryKey);
-
     @Query("SELECT * FROM game WHERE game_id LIKE :gameId LIMIT 1")
     Game findByGameId(String gameId);
 
